f12bae6d82c1298bbcf24f02f43072beea2e7da7
Tallyon <tallyon.lima@sptech.school>
2026-08-27 19:59:20 -0300

    Style: I adjusted the screen layout to prevent the NavBar from scrolling

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,28 +5,55 @@ import Footer from "../components/Footer/Footer";
 import SideBar from "../components/SideBar/SideBar";
 
 const AppContainer = styled.div`
-  display: flex;
   min-height: 100vh;
 `;
 
 const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  min-height: 100vh;
+  margin-left: 240px;
 `;
 
 const FooterContainer = styled.div`
   margin-top: auto;
 `;
 
+const SideBarContainer = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    display: flex;
+    flex-direction: column;
+
+    width: 240px;
+    height: 100vh;
+
+    box-sizing: border-box;
+    padding: 20px;
+
+    font-family: Merriweather;
+    background-color: var(--azul-mais-escuro);
+
+    overflow-x: hidden;
+    overflow-y: auto;
+    z-index: 1000;
+`;
+
+const PageContent = styled.div`
+  min-height: 100vh;
+`;
+
 function MainLayout() {
   return (
     <AppContainer>
-      <SideBar />
+      <SideBarContainer>
+        <SideBar />
+      </SideBarContainer>
 
       <Main>
-        <Outlet />
+
+        <PageContent>
+          <Outlet />
+        </PageContent>
 
         <FooterContainer>
           <Footer />
