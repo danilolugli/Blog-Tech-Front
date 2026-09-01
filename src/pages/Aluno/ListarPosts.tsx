89ec00d7f8eab688e4fc3081f61d0b3a192b2539
Danilo <daniloluglisantos@gmail.com>
2026-08-31 22:41:27 -0300

    melhorias tela listar posts

--- src/pages/Aluno/ListarPosts.tsx
+++ src/pages/Aluno/ListarPosts.tsx
@@ -2,44 +2,51 @@ import styled from 'styled-components';
 import './ListarPosts.css';
 import { PostFormModal } from '../../components/PostFormModal/PostFormModal';
 import PostPreview from '../../components/PostPreview/PostPreview';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const ListarPosts: React.FC = () =>  {
   return (
     <>
       <h1>Posts Recentes</h1>
+      <header className='containerPesquisa'>   
+        <FontAwesomeIcon icon={faMagnifyingGlass} className='iconePesquisa' />
         <input type="text" placeholder="Busque por palavras-chave" className="inputPesquisa" />
-
+      </header>
+      
+      <div className='btnCriarPost'>
         <PostFormModal authorName={''} onSubmit={function (values: { title: string; content: string; subject: string; }, postId?: string): Promise<void> | void {
-        throw new Error('Function not implemented.');
-      } }></PostFormModal>
+          throw new Error('Function not implemented.');
+        } }></PostFormModal>
+      </div>
 
-        <ul className='listaPosts'>
-          <li>
-            <PostPreview titulo={'10 Dinâmicas de Grupo Práticas para Voltar às Aulas com Energia'} 
-            descricao={'Descubra dinâmicas de acolhimento fáceis de aplicar no ensino fundamental e médio. Promova a empatia e quebre o gelo nos primeiros dias do semestre letivo com material de apoio exclusivo.'} />
-          </li>
-          <li>
-            <PostPreview titulo={'10 Dinâmicas de Grupo Práticas para Voltar às Aulas com Energia'} 
-            descricao={'Descubra dinâmicas de acolhimento fáceis de aplicar no ensino fundamental e médio. Promova a empatia e quebre o gelo nos primeiros dias do semestre letivo com material de apoio exclusivo.'} />
-          </li>
-          <li>
-            <PostPreview titulo={'10 Dinâmicas de Grupo Práticas para Voltar às Aulas com Energia'} 
-            descricao={'Descubra dinâmicas de acolhimento fáceis de aplicar no ensino fundamental e médio. Promova a empatia e quebre o gelo nos primeiros dias do semestre letivo com material de apoio exclusivo.'} />
-          </li>
-             <li>
-            <PostPreview titulo={'10 Dinâmicas de Grupo Práticas para Voltar às Aulas com Energia'} 
-            descricao={'Descubra dinâmicas de acolhimento fáceis de aplicar no ensino fundamental e médio. Promova a empatia e quebre o gelo nos primeiros dias do semestre letivo com material de apoio exclusivo.'} />
-          </li>
-          <li>
-            <PostPreview titulo={'10 Dinâmicas de Grupo Práticas para Voltar às Aulas com Energia'} 
-            descricao={'Descubra dinâmicas de acolhimento fáceis de aplicar no ensino fundamental e médio. Promova a empatia e quebre o gelo nos primeiros dias do semestre letivo com material de apoio exclusivo.'} />
-          </li>
-          <li>
-            <PostPreview titulo={'10 Dinâmicas de Grupo Práticas para Voltar às Aulas com Energia'} 
-            descricao={'Descubra dinâmicas de acolhimento fáceis de aplicar no ensino fundamental e médio. Promova a empatia e quebre o gelo nos primeiros dias do semestre letivo com material de apoio exclusivo.'} />
-          </li>
-        </ul>
+      <ul className='listaPosts'>
+        <li>
+          <PostPreview titulo={'10 Dinâmicas de Grupo Práticas para Voltar às Aulas com Energia'} 
+          descricao={'Descubra dinâmicas de acolhimento fáceis de aplicar no ensino fundamental e médio. Promova a empatia e quebre o gelo nos primeiros dias do semestre letivo com material de apoio exclusivo.'} />
+        </li>
+        <li>
+          <PostPreview titulo={'10 Dinâmicas de Grupo Práticas para Voltar às Aulas com Energia'} 
+          descricao={'Descubra dinâmicas de acolhimento fáceis de aplicar no ensino fundamental e médio. Promova a empatia e quebre o gelo nos primeiros dias do semestre letivo com material de apoio exclusivo.'} />
+        </li>
+        <li>
+          <PostPreview titulo={'10 Dinâmicas de Grupo Práticas para Voltar às Aulas com Energia'} 
+          descricao={'Descubra dinâmicas de acolhimento fáceis de aplicar no ensino fundamental e médio. Promova a empatia e quebre o gelo nos primeiros dias do semestre letivo com material de apoio exclusivo.'} />
+        </li>
+            <li>
+          <PostPreview titulo={'10 Dinâmicas de Grupo Práticas para Voltar às Aulas com Energia'} 
+          descricao={'Descubra dinâmicas de acolhimento fáceis de aplicar no ensino fundamental e médio. Promova a empatia e quebre o gelo nos primeiros dias do semestre letivo com material de apoio exclusivo.'} />
+        </li>
+        <li>
+          <PostPreview titulo={'10 Dinâmicas de Grupo Práticas para Voltar às Aulas com Energia'} 
+          descricao={'Descubra dinâmicas de acolhimento fáceis de aplicar no ensino fundamental e médio. Promova a empatia e quebre o gelo nos primeiros dias do semestre letivo com material de apoio exclusivo.'} />
+        </li>
+        <li>
+          <PostPreview titulo={'10 Dinâmicas de Grupo Práticas para Voltar às Aulas com Energia'} 
+          descricao={'Descubra dinâmicas de acolhimento fáceis de aplicar no ensino fundamental e médio. Promova a empatia e quebre o gelo nos primeiros dias do semestre letivo com material de apoio exclusivo.'} />
+        </li>
+      </ul>
     </>
   );
 }
